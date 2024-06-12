bb52202295cb48944387f4be7ae7981203bfc24a
Totorokoto <pacueto08@gmail.com>
2024-06-12 20:02:05 +0800

    Add files via upload

---
+++ Admin/header1.php.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "../css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "../css/style.css" />
+	<style>
+		.navbar {
+			background-color: #2c3e50;
+			border: none;
+			border-radius: 0;
+		}
+
+		.navbar-brand {
+			color: #ecf0f1 !important;
+			font-size: 24px;
+			font-weight: bold;
+		}
+
+		#menu {
+			list-style: none;
+			padding: 0;
+			text-align: center;
+			background-color: #34495e;
+			margin: 0;
+			font-size: 18px;
+		}
+
+		#menu li {
+			display: inline;
+			margin-right: 15px;
+		}
+
+		#menu li a {
+			text-decoration: none;
+			color: #ecf0f1;
+			padding: 10px 15px;
+			display: inline-block;
+		}
+
+		#menu li a:hover {
+			background-color: #2980b9;
+			border-radius: 5px;
+		}
+	</style>
+
+	
+
+
+
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+      
+				<a class="navbar-brand">DEFENSE HOTEL PH.</a>
+			</div>
+		</div>
+	</nav>
+
+	</div>
+	<br />
